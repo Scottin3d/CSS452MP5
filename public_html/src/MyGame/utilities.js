9b74098725f4f95f3c6d56ce2b38958f28e13bd3
Scottin3d <scottin3d@gmail.com>
2021-02-18 16:17:22 -0800

    addes utilities.js

--- public_html/src/MyGame/utilities.js
+++ public_html/src/MyGame/utilities.js
@@ -6,6 +6,12 @@ font32 = "assets/fonts/Consolas-32";
 font24 = "assets/fonts/Consolas-24";
 font16 = "assets/fonts/Consolas-16";
 
+
+/// <summary>
+/// Converts a hexadecimal color to RGBA 1
+/// </summary>
+/// <param name="hex">The hexadecimal to be converted.</param>
+/// <returns>A dictionary of 0-1 RGBA values.</returns>
 function hexToRgb(hex) {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ? {
